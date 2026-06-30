52e46170eff97dd2e2d58091f97fbe6f4c312b1f
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2026-06-30 16:39:50 +0100

    Update display on documentation

--- previews/header_component_preview.rb
+++ previews/header_component_preview.rb
@@ -4,18 +4,12 @@ class HeaderComponentPreview < ViewComponent::Preview
   # More information about how and when to use the [MoJ Header is available in the MoJ
   # Design System documentation](https://design-patterns.service.justice.gov.uk/components/moj-header/)
   #
-  # @param organisation_name text
-  # @param url text
-  def default(organisation_name: "Organisation Name", url: "#")
-    render_with_template(locals: { organisation_name: organisation_name, url: url })
+  def default
+    render_with_template
   end
 
-  # @param organisation_name text
-  # @param url text
-  # @param nav_item_1 text
-  # @param nav_item_2 text
-  def with_navigation_items(organisation_name: "Organisation Name", url: "#", nav_item_1: "Nav item 1", nav_item_2: "Nav item 2")
-    render_with_template(locals: { organisation_name:, url:, nav_item_1:, nav_item_2: })
+  def with_navigation_items
+    render_with_template
   end
 
   # @!endgroup
